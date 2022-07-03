08bf30ac047a0f6441d096bbbed189bf4fda98dc
Hackante <kante.hagen@gmail.com>
2022-07-03 15:51:00 +0200

    removed comments

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,14 @@
-#include <Wire.h>                   // Wire Bibliothek einbinden
-#include <LiquidCrystal_I2C.h>      // Vorher hinzugefügte LiquidCrystal_I2C Bibliothek einbinden
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall eines mit 16 Zeichen in 2 Zeilen und der HEX-Adresse 0x27. Für ein vierzeiliges I2C-LCD verwendet man den Code "LiquidCrystal_I2C lcd(0x27, 20, 4)"
+#include <Wire.h>                   
+#include <LiquidCrystal_I2C.h>      
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
 int buttonPin = 7;
 int tonePin = 4;
 
 void setup()
 {
-  lcd.init();      // Im Setup wird der LCD gestartet
-  lcd.backlight(); // Hintergrundbeleuchtung einschalten (lcd.noBacklight(); schaltet die Beleuchtung aus).
+  lcd.init();      
+  lcd.backlight(); 
   pinMode(buttonPin, INPUT);
   pinMode(tonePin, OUTPUT);
   startMenue();
@@ -18,9 +18,9 @@ void setup()
 void loop()
 {
   lcd.clear();
-  lcd.setCursor(0, 0); // Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile.
+  lcd.setCursor(0, 0); 
   lcd.print("Wait for");
-  lcd.setCursor(0, 1); // In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile.
+  lcd.setCursor(0, 1); 
   lcd.print("the tone...");
   delay(random(1000, 5000));
 
